794cef78228243fda624e7863c20aa30939c2637
Dario DAngelo <dariodangelo21@gmail.com>
2022-12-24 13:03:48 -0300

    autogenerate keys false

--- app/src/main/java/com/example/learnandroiddevelopment/domain/model/KotlinModel.kt
+++ app/src/main/java/com/example/learnandroiddevelopment/domain/model/KotlinModel.kt
@@ -1,10 +1,12 @@
 package com.example.learnandroiddevelopment.domain.model
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.example.learnandroiddevelopment.util.Constants.KOTLIN_DATABASE
 
 @Entity(tableName = KOTLIN_DATABASE )
 data class KotlinModel(
+    @PrimaryKey(autoGenerate = false)
     val id: Int,
     val title: String,
     val image: String,
